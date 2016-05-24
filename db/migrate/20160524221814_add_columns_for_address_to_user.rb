28d33a083bf84d799252ecc0d1fb5d4873c730f4
Valerie <val.conklin@gmail.com>
2016-05-24 15:25:31 -0700

    Users now have street address, city, and state just like orders

---
+++ db/migrate/20160524221814_add_columns_for_address_to_user.rb
@@ -0,0 +1,7 @@
+class AddColumnsForAddressToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :street_address, :string
+    add_column :users, :city, :string
+    add_column :users, :state, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160524215250) do
+ActiveRecord::Schema.define(version: 20160524221814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,6 +90,9 @@ ActiveRecord::Schema.define(version: 20160524215250) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "street_address"
+    t.string   "city"
+    t.string   "state"
   end
 
 end
